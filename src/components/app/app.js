f398a5341d0c1487391a0101bec1297477e0ca0d
Roman Morozov <ramorozov1984@gmail.com>
2023-03-30 23:58:34 +0700

    added search-panel components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,15 @@
-import './app.css'
+import './app.css';
 import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
 
 function App() {
     return (
         <div className="app">
             <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+            </div>
         </div>
     );
 }

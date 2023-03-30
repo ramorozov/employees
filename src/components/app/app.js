243d01f8ac591a41dc70bf53d9f9fd3e7f33ea1d
Roman Morozov <ramorozov1984@gmail.com>
2023-03-30 23:45:39 +0700

    added app-info components

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import './app.css'
+import AppInfo from '../app-info/app-info';
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo/>
+        </div>
+    );
+}
+
+export default App;

d9b9013cd93f821167cf0accb3841e0294cf645d
Roman Morozov <ramorozov1984@gmail.com>
2023-04-12 17:07:42 +0700

    added EmployeesList & EmployeesAddForm components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,8 @@ import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
-import EmployeesListItem from '../employees-list-item/employees-list-item';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
     return (
@@ -12,8 +13,10 @@ function App() {
             <div className="search-panel">
                 <SearchPanel/>
                 <AppFilter/>
-                <EmployeesListItem/>
             </div>
+
+            <EmployeesList/>
+            <EmployeesAddForm/>
         </div>
     );
 }

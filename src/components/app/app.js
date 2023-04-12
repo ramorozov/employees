046f1de64f20cef16c73480efee687f713ab0d0e
Roman Morozov <ramorozov1984@gmail.com>
2023-04-12 16:35:40 +0700

    added employees-list-item components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <div className="search-panel">
                 <SearchPanel/>
                 <AppFilter/>
+                <EmployeesListItem/>
             </div>
         </div>
     );
